8f2bae241e5cdec832a0976307477903b646958e
danilocouto1 <dansantosalfa@gmail.com>
2021-10-04 22:38:42 -0300

    Criando um header atraves do style components

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    #background-color: #17181F;
+    background-color: #17181F;
     color: #797A81;
     min-height: 100vh;
 `;
@@ -10,4 +10,13 @@ export const Area = styled.div`
    margin: auto;
    max-width: 980px;
    padding: 10px;
+`;
+
+export const Header = styled.h1`
+   margin: 0;
+   padding: 0;
+   color: #FFF;
+   text-align: center;
+   border-bottom: 1px solid #444;
+   padding-bottom: 20px;
 `;
