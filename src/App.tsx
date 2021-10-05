542b0ec7fedf37402ea41a0cca4435b92f986ba3
danilocouto1 <dansantosalfa@gmail.com>
2021-10-04 22:40:19 -0300

    Adicionando useState ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import * as Componontens from './App.styles'
 
 const App = () => {
   return (
     <Componontens.Container>
-      
+      <Componontens.Area>
+        <Componontens.Header>Lista de Tarefas de Danilo</Componontens.Header>
+      </Componontens.Area>
     </Componontens.Container>
   );
 }
