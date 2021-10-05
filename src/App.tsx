c3d7381e5bd79f1c7ba9bdf9bfe856ffa07c728e
danilocouto1 <dansantosalfa@gmail.com>
2021-10-04 23:16:01 -0300

    Import o ListIntens e render pelo map

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import * as Componontens from './App.styles'
 import { item } from "./types/item";
+import { ListItem } from "./components/ListItens";
 
 const App = () => {
   const [list, setList] = useState<item[]>([])
@@ -9,6 +10,10 @@ const App = () => {
     <Componontens.Container>
       <Componontens.Area>
         <Componontens.Header>Lista de Tarefas de Danilo</Componontens.Header>
+
+        {list.map((item, index) => {
+          <ListItem key={index} itemProps={item}/>
+        })}
       </Componontens.Area>
     </Componontens.Container>
   );
