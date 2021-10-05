c8129b16122075e31be630780ad291f92bf15177
danilocouto1 <dansantosalfa@gmail.com>
2021-10-04 23:52:28 -0300

    criando a função handleTaskChange

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,28 @@ import { ListItem } from "./components/ListItens";
 const App = () => {
   const [list, setList] = useState<item[]>([])
   
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+  
   return (
     <Componontens.Container>
       <Componontens.Area>
         <Componontens.Header>Lista de Tarefas de Danilo</Componontens.Header>
 
-        {list.map((item, index) => {
-          <ListItem key={index} itemProps={item}/>
-        })}
+        {list.map((item, index)=>(
+            <ListItem
+              key={index}
+              itemProps={item}
+              onChange={handleTaskChange}
+            />
+          ))}
       </Componontens.Area>
     </Componontens.Container>
   );
